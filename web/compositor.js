8d0fb70df32d9faf443abf160ae2ae5a22991084
Enrico <enrico.furlan@gmail.com>
2024-08-29 17:41:36 +0200

    work in progress

--- web/compositor.js
+++ web/compositor.js
@@ -9,13 +9,18 @@ const COMPOSITOR = Symbol();
 const TEST_IMAGE_2 = "./extensions/ComfyUI-enricos-nodes/empty.png"
 
 var stuff = {
-    canvas: null,
-    // image1: null,
-    // image2: null,
-    // image3: null,
-    // image4: null,
-    safeAreaBorder: null,
+    shared:
+        {
+            canvas: null,
+            // image1: null,
+            // image2: null,
+            // image3: null,
+            // image4: null,
+            safeAreaBorder: null,
+        }
 }
+
+
 app.registerExtension({
     name: "Comfy.Compositor",
     getCustomWidgets(app) {
@@ -43,33 +48,33 @@ app.registerExtension({
         // and leverage theImage.cacheKey if it's the same image, do nothing
         function addOrReplace(theImage, index) {
 
-            if (stuff['image' + (index + 1)] == null) {
-                stuff['image' + (index + 1)] = theImage;
-                stuff.canvas.add(theImage);
+            if (stuff.shared['image' + (index + 1)] == null) {
+                stuff.shared['image' + (index + 1)] = theImage;
+                stuff.shared.canvas.add(theImage);
                 console.log("add or replace", index + 1, "isNull");
             }
 
-            if (stuff['image' + (index + 1)] != null) {
+            if (stuff.shared['image' + (index + 1)] != null) {
 
                 const oldTransform = {
-                    left: stuff['image' + (index + 1)].left,
-                    top: stuff['image' + (index + 1)].top,
-                    scaleX: stuff['image' + (index + 1)].scaleX,
-                    scaleY: stuff['image' + (index + 1)].scaleY,
-                    angle: stuff['image' + (index + 1)].angle,
-                    flipX: stuff['image' + (index + 1)].flipX,
-                    flipY: stuff['image' + (index + 1)].flipY,
-                    originX: stuff['image' + (index + 1)].originX,
-                    originY: stuff['image' + (index + 1)].originY,
+                    left: stuff.shared['image' + (index + 1)].left,
+                    top: stuff.shared['image' + (index + 1)].top,
+                    scaleX: stuff.shared['image' + (index + 1)].scaleX,
+                    scaleY: stuff.shared['image' + (index + 1)].scaleY,
+                    angle: stuff.shared['image' + (index + 1)].angle,
+                    flipX: stuff.shared['image' + (index + 1)].flipX,
+                    flipY: stuff.shared['image' + (index + 1)].flipY,
+                    originX: stuff.shared['image' + (index + 1)].originX,
+                    originY: stuff.shared['image' + (index + 1)].originY,
                 };
 
                 //Remove the old image from the canvas
-                stuff.canvas.remove(stuff['image' + (index + 1)]);
+                stuff.shared.canvas.remove(stuff.shared['image' + (index + 1)]);
                 theImage.set(oldTransform);
-                stuff.canvas.add(theImage);
-                stuff['image' + (index + 1)] = theImage;
+                stuff.shared.canvas.add(theImage);
+                stuff.shared['image' + (index + 1)] = theImage;
             }
-            stuff.canvas.bringToFront(stuff.safeAreaBorder)
+            stuff.shared.canvas.bringToFront(stuff.shared.safeAreaBorder)
         }
 
         function imageMessageHandler(event) {
@@ -81,37 +86,37 @@ app.registerExtension({
                 fabric.Image.fromURL(b64, function (oImg) {
                     addOrReplace(oImg, index);
                 });
-                // stuff.canvas.renderAll();
+                // stuff.shared.canvas.renderAll();
             });
         }
 
         api.addEventListener("compositor.images", imageMessageHandler);
     },
-    loadedGraphNode(node,app){
-        node.type =="Compositor" && console.log(node,node["stuff"]);
+    loadedGraphNode(node, app) {
+        node.type == "Compositor" && console.log(node, node["stuff"]["shared"]);
 
-        const ns = node.stuff;
-            ns.safeArea.setHeight(ns.h.value);
-            ns.safeArea.setWidth(ns.w.value);
-            ns.safeArea.setLeft(ns.p.value);
-            ns.safeArea.setTop(ns.p.value);
+        const ns = node.stuff.shared;
+        ns.safeArea.setHeight(ns.h.value);
+        ns.safeArea.setWidth(ns.w.value);
+        ns.safeArea.setLeft(ns.p.value);
+        ns.safeArea.setTop(ns.p.value);
 
-            ns.safeAreaBorder.setHeight(ns.h.value +2);
-            ns.safeAreaBorder.setWidth(ns.w.value +2);
-            ns.safeAreaBorder.setLeft(ns.p.value -1);
-            ns.safeAreaBorder.setTop(ns.p.value -1 );
-            ns.safeAreaBorder.set("strokeWidth",1);
-            ns.safeAreaBorder.set("stroke", "#00b300b0");
-            ns.safeAreaBorder.bringToFront()
+        ns.safeAreaBorder.setHeight(ns.h.value + 2);
+        ns.safeAreaBorder.setWidth(ns.w.value + 2);
+        ns.safeAreaBorder.setLeft(ns.p.value - 1);
+        ns.safeAreaBorder.setTop(ns.p.value - 1);
+        ns.safeAreaBorder.set("strokeWidth", 1);
+        ns.safeAreaBorder.set("stroke", "#00b300b0");
+        ns.safeAreaBorder.bringToFront()
 
-            ns.canvas.bringToFront(ns.safeAreaBorder);
+        ns.canvas.bringToFront(ns.safeAreaBorder);
 
-            //console.log(v.getWidth(), v.getHeight(), value);
-            ns.v.setHeight(ns.safeArea.getHeight() + (ns.p.value * 2));
-            ns.v.setWidth(ns.safeArea.getWidth() + (ns.p.value * 2));
-            ns.v.renderAll();
-            ns.node.setSize([ns.v.getWidth() + 100, ns.v.getHeight() + 556]);
-            ns.capture();
+        //console.log(v.getWidth(), v.getHeight(), value);
+        ns.v.setHeight(ns.safeArea.getHeight() + (ns.p.value * 2));
+        ns.v.setWidth(ns.safeArea.getWidth() + (ns.p.value * 2));
+        ns.v.renderAll();
+        ns.node.setSize([ns.v.getWidth() + 100, ns.v.getHeight() + 556]);
+        ns.capture();
 
     },
     nodeCreated(node) {
@@ -139,7 +144,7 @@ app.registerExtension({
             selectionColor: 'transparent',
             selectionLineWidth: 1,
             // F-10 preserve object stacking
-            preserveObjectStacking:true,
+            preserveObjectStacking: true,
         });
 
 
@@ -154,15 +159,15 @@ app.registerExtension({
         });
 
         var safeAreaBorder = new fabric.Rect({
-            left: p.value -1,
-            top: p.value -1,
+            left: p.value - 1,
+            top: p.value - 1,
             fill: 'rgba(0,0,0,0.0)',
-            width: w.value +1,
-            height: h.value +1,
+            width: w.value + 1,
+            height: h.value + 1,
             selectable: false,
         });
 
-        stuff.safeAreaBorder = safeAreaBorder;
+        stuff.shared.safeAreaBorder = safeAreaBorder;
 
         v.add(safeArea);
         v.add(safeAreaBorder);
@@ -184,7 +189,7 @@ app.registerExtension({
             //h.origCalback(value, graphCanvas, node, pos, event);
             v.setHeight(value + (p.value * 2));
             safeArea.setHeight(value);
-            safeAreaBorder.setHeight(value +2);
+            safeAreaBorder.setHeight(value + 2);
             v.renderAll();
             node.setSize([v.getWidth() + 100, v.getHeight() + 556])
         }
@@ -201,8 +206,8 @@ app.registerExtension({
 
             safeAreaBorder.setHeight(h.value + 2);
             safeAreaBorder.setWidth(w.value + 2);
-            safeAreaBorder.setLeft(value -1);
-            safeAreaBorder.setTop(value -1);
+            safeAreaBorder.setLeft(value - 1);
+            safeAreaBorder.setTop(value - 1);
 
 
             //console.log(v.getWidth(), v.getHeight(), value);
@@ -213,11 +218,10 @@ app.registerExtension({
         }
 
         /** the fabric canvas:v */
-        stuff.canvas = v;
+        stuff.shared.canvas = v;
 
         // final image to be associated to the node preview
         const img = new Image();
-
 
 
         // data url
@@ -248,17 +252,17 @@ app.registerExtension({
         capture();
 
         // grab some references in the node. hopefully they are not serialized :D
-        node.stuff = {
-            p:p,
-            w:w,
-            h:h,
+        node.stuff.shared = {
+            p: p,
+            w: w,
+            h: h,
             /** the fabric canvas:v */
-            v:v,
-            camera:camera,
-            i:img,
-            safeArea:safeArea,
-            safeAreaBorder:safeAreaBorder,
-            capture:capture,
+            v: v,
+            camera: camera,
+            i: img,
+            safeArea: safeArea,
+            safeAreaBorder: safeAreaBorder,
+            capture: capture,
         }
 
         const btn = node.addWidget("button", "capture", "capture", capture);

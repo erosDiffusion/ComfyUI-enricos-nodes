dd6b2c604c452a58d0462d474284c00f7c9e54a2
Enrico <enrico.furlan@gmail.com>
2024-09-18 13:54:41 +0200

    fix swapped width and height

--- web/compositor3.js
+++ web/compositor3.js
@@ -673,8 +673,8 @@ class Editor {
             flipY: this.inputImages[this.imageNameAt(index)].flipY,
             originX: this.inputImages[this.imageNameAt(index)].originX,
             originY: this.inputImages[this.imageNameAt(index)].originY,
-            xwidth: this.inputImages[this.imageNameAt(index)].height,
-            xheight: this.inputImages[this.imageNameAt(index)].width,
+            xwidth: this.inputImages[this.imageNameAt(index)].width,
+            xheight: this.inputImages[this.imageNameAt(index)].height,
             skewY: this.inputImages[this.imageNameAt(index)].skewY,
             skewX: this.inputImages[this.imageNameAt(index)].skewX,
         };
